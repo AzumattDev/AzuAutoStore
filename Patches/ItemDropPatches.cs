0212baca9b158bb209b58cec04798d970bf6ba09
AzumattDev <azumattgaming@gmail.com>
2023-11-02 09:04:24 -0400

    v2.1.3: NRE Fix

--- Patches/ItemDropPatches.cs
+++ Patches/ItemDropPatches.cs
@@ -10,12 +10,24 @@ static class ItemDropStartPatch
     static void Postfix(ItemDrop __instance)
     {
         AzuAutoStorePlugin.AzuAutoStoreLogger.LogDebug("ItemDrop Start");
+        if (Boxes.Containers == null || __instance == null || __instance.transform == null)
+        {
+            return;
+        }
+
         for (int index = 0; index < Boxes.Containers.Count; ++index)
         {
-            Container container = Boxes.Containers[index];
+            Container? container = Boxes.Containers[index];
+            if (container == null || container.transform == null)
+            {
+                continue;
+            }
+
             if (ItemDrop.s_instances.Count == ContainerAwakePatch.lastCount) continue;
+
             float distance = Vector3.Distance(container.transform.position, __instance.transform.position);
-            if (!(distance <= Functions.GetContainerRange(container))) continue;
+            if (distance > Functions.GetContainerRange(container)) continue;
+
             ContainerAwakePatch.ItemDroppedNearby(container);
             AzuAutoStorePlugin.AzuAutoStoreLogger.LogDebug($"ItemDrop s_instances count changed from {ContainerAwakePatch.lastCount} to {ItemDrop.s_instances.Count}");
             ContainerAwakePatch.lastCount = ItemDrop.s_instances.Count;
--- Plugin.cs
+++ Plugin.cs
@@ -20,7 +20,7 @@ namespace AzuAutoStore
     public class AzuAutoStorePlugin : BaseUnityPlugin
     {
         internal const string ModName = "AzuAutoStore";
-        internal const string ModVersion = "2.1.2";
+        internal const string ModVersion = "2.1.3";
         internal const string Author = "Azumatt";
         internal const string ModGUID = $"{Author}.{ModName}";
         private static readonly string ConfigFileName = ModGUID + ".cfg";
